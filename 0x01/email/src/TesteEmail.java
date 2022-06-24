39f8088f0a137f0bf8364a5380001b21c85be125
Letícia Leonel Rodrigues <CIANDT\leticial@lnb036111cps.ciandt.global>
2022-06-24 09:33:37 -0300

    Task 0 - JUnit

---
+++ 0x01/email/src/TesteEmail.java
@@ -0,0 +1,26 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class TesteEmail {
+
+        @Test
+        public void testar_email_com_arroba() {
+            assertTrue(Pessoa.emailValid("email_teste@dominio.com.br"));
+        }
+
+        @Test
+        public void testar_email_sem_arroba() {
+            assertFalse(Pessoa.emailValid("email_testedominio.com.br"));
+        }
+
+        @Test
+        public void testar_email_mais_50_caracteres() {
+            assertEquals(false, Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br"));
+        }
+
+}
+
+
+
